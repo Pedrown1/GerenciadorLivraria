17636d79827d4af46a943f8c450b8e2bbd35ca18
Pedro Henrique Silva Carvalho <pedro.carvalho@doalti.corp>
2024-12-30 09:30:37 -0300

    Primeiro Commit: Estrutura do Projeto

---
+++ src/main/java/com/ph/project/GerenciadorLivraria/GerenciadorLivrariaApplication.java
@@ -0,0 +1,13 @@
+package com.ph.project.GerenciadorLivraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorLivrariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorLivrariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ph/project/GerenciadorLivraria/GerenciadorLivrariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ph.project.GerenciadorLivraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorLivrariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
